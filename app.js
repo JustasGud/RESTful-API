03dadf6a1712828bd1699055dc159ad4ed10c90f
Justas Gudonis <gudonisjustas@gmail.com>
2022-07-03 13:44:41 +0300

    updated code with chained route handlers

--- app.js
+++ app.js
@@ -26,37 +26,49 @@ const articleSchema = new mongoose.Schema({
 // creating article model
 const Article = mongoose.model("Article", articleSchema);
 
-// Created get request to get all articles from the database
-app.get("/articles", function (req, res) {
-  Article.find(function (err, foundArticles) {
-    if (!err) {
-      //console.log(foundArticles);
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-});
+//updated code with chained route handlers
+app
+  .route("/articles")
+  // Created get request to get all articles from the database
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        //console.log(foundArticles);
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  //Testing POST request data via postman
+  .post(function (req, res) {
+    //console.log(req.body.title);
+    //console.log(req.body.content);
 
-//Testing POST request data via postman
-app.post("/articles", function (req, res) {
-  //console.log(req.body.title);
-  //console.log(req.body.content);
+    //Saving data in the database
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
 
-  //Saving data in the database
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfuly added new article!");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  //Delete all articles from database
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfuly deleted all articles!");
+      } else {
+        res.send(err);
+      }
+    });
   });
-
-  newArticle.save(function (err) {
-    if (!err) {
-      res.send("Successfuly added new article!");
-    } else {
-      res.send(err);
-    }
-  });
-});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
